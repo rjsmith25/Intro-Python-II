3074b2a8d7869891c4be252ab60e52cf1e4653ae
Russell Smith <russellsmith@MacBook-Pro.local>
2020-08-11 21:44:32 -0400

    day 1 mvp completed, started on day 2 already soon complete everything

--- src/player.py
+++ src/player.py
@@ -1,2 +1,23 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+        self.inventory = []
+
+    def getInventoryItem(self,name):
+        for i in range(0, len(self.inventory)):
+            if(self.inventory[i].name == name):
+                return self.inventory[i]
+
+    def addInventoryItem(self,item):
+        self.inventory.append(item)
+
+    def removeInventoryItem(self, item):
+        index = None
+        for i in range(0, len(self.inventory)):
+            if(self.inventory[i].name == item.name):
+                index = i
+        if(index != None):
+            self.inventory.pop(index)
--- src/room.py
+++ src/room.py
@@ -1,2 +1,28 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.items = []
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to= None
+
+    def addItem(self,item):
+        self.items.append(item)
+
+    def getItem(self,name):
+        for i in range(0, len(self.items)):
+            if(self.items[i].name == name):
+                return self.items[i]
+
+
+    def removeItem(self,item):
+        index = None
+        for i in range(0, len(self.items)):
+            if(self.items[i].name == item.name):
+                index = i
+        if(index != None):
+            self.items.pop(index)
